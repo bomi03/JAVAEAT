f475cae561f63c7bb07825a278be77d21594f7c7
chaebin1207 <chaebin1207@sookmyung.ac.kr>
2025-05-07 21:43:10 +0900

    message 완료

--- src/model/Comment.java
+++ src/model/Comment.java
@@ -1,13 +1,57 @@
 package model;
 
 public class Comment {
-    private int commentID;
-    private int postID;
-    private String writerID;
-    private String content;
-    private int parentCommentID;
+    private int commentID;          
+    private int postID;             
+    private String writerID;        
+    private String content;        
+    private Integer parentCommentID; 
 
-    public void addComment(String content) {}
-    public void addReply(Comment parent, String replyContent) {}
-    public void deleteComment() {}
+    public Comment(int commentID, int postID, String writerID, String content, Integer parentCommentID) {
+        this.commentID = commentID;
+        this.postID = postID;
+        this.writerID = writerID;
+        this.content = content;
+        this.parentCommentID = parentCommentID;
+    }
+
+    // get
+    public int getCommentID() {
+        return commentID;
+    }
+
+    public int getPostID() {
+        return postID;
+    }
+
+    public String getWriterID() {
+        return writerID;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Integer getParentCommentID() {
+        return parentCommentID;
+    }
+
+    // set
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    // 기능 메서드
+
+    public static Comment addComment(int commentID, int postID, String writerID, String content) {
+        return new Comment(commentID, postID, writerID, content, null);
+    }
+
+    public static Comment addReply(int commentID, int postID, String writerID, String content, int parentCommentID) {
+        return new Comment(commentID, postID, writerID, content, parentCommentID);
+    }
+
+    public void deleteComment() {
+        this.content = "[삭제된 댓글입니다]";
+    }
 }
