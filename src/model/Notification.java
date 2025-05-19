ac1dc9b325a249235131b9a5a107d1980173a393
onemorelight1 <labyrinth103@gmail.com>
2025-05-19 23:28:58 +0900

    NotificationType 분리
    NotificationType 분리

--- src/model/Notification.java
+++ src/model/Notification.java
@@ -1,11 +1,3 @@
-// 알림 유형 enum
-public enum NotificationType {
-    APPLY,    // 지원
-    ACCEPT,   // 수락
-    REJECT,   // 거절
-    MESSAGE   // 메시지
-}
-
 // 알림 정보 클래스
 public class Notification {
     private final int notificationId;
